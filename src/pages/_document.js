46923839aeb5210cc35b057ac41357a3ebdd7147
willz <willzittlau@outlook.com>
2021-05-06 13:07:36 -0700

    remove mailchimp

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -16,18 +16,6 @@ class CustomDocument extends Document {
             as="font"
             crossOrigin=""
           />
-          {/*mailchimp*/}
-          <script id="mcjs">
-            !function(c,h,i,m,p)
-            {
-              ((m = c.createElement(h)),
-              (p = c.getElementsByTagName(h)[0]),
-              (m.async = 1),
-              (m.src = i),
-              p.parentNode.insertBefore(m, p))
-            }
-            (document,"script","https://chimpstatic.com/mcjs-connected/js/users/48162a270c3eabb026c9e3be0/6c634a006c6793fb2aac5cb8d.js");
-          </script>
         </Head>
         <body>
           <Main />
